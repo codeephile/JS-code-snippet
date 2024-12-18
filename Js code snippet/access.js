330aea3e2baff8177d9509e9c20643317a7e4eb0
G <dream.creater96@gmail.com>
2024-12-18 11:10:16 -0500

    access.js

--- Js code snippet/access.js
+++ Js code snippet/access.js
@@ -38,3 +38,34 @@ async function doTask(){
     }
 }
 doTask();
+
+
+// while loop //
+var logged = false;
+var user;
+var key;
+var count = 1;
+
+while(!logged){
+    user = prompt("Enter Name");
+    key = prompt("Enter Key");
+
+    if(user && key){
+        logged = true;
+        console.log("User Accessed");
+    }else{
+        console.log("Access Denied");
+    }
+}
+const countAuto = () => {
+    if(logged){
+        console.log("Timing: ", count);
+        count++;
+    }
+    if(count === 10){
+        console.log("Boom!");
+        return;
+    }
+    setTimeout(countAuto, 1200);
+}
+countAuto();
