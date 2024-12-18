bc1d7ab40de630dc35afd1af991b821551db4eae
G <dream.creater96@gmail.com>
2024-12-18 10:11:30 -0500

    loop.js

--- Js code snippet/loop.js
+++ Js code snippet/loop.js
@@ -1,3 +1,4 @@
+// forEach html //
 var username = ['alice', 'lewis', 'katy', 'david', 'johnson'];
 var country = ['us', 'canada', 'japan', 'korea', 'russia'];
 let delay = 0;
@@ -9,3 +10,29 @@ username.forEach((name, index) => {
     }, delay);
 });
 
+---------------------------------------------------------
+// forEach //
+let user = ['alice', 'lewis'];
+let key = [1001, 1002];
+let delay = 0;
+
+user.forEach((name, index) => { 
+  delay += 2000;
+  setTimeout(() => {
+    console.log(`Hello ${name} , Your access key is ${key[index]}`);
+  }, delay);
+});
+
+// for all //
+var username = ['alice', 'lewis', 'johnson'];
+var city = ['seoul', 'us', 'tokyo'];
+
+username.forEach((name, index) => {
+    users(name, city[index]);
+});
+
+function users(name, town){
+    setTimeout(() => {
+        console.log(`Welcome ${name}, You are from ${town}`);
+    }, 3000);
+}
