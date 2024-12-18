fe7da3da4ce0dcade03ef3e31f28b393e2d6996f
G <dream.creater96@gmail.com>
2024-12-18 09:45:32 -0500

    forEach.js

--- Js code snippet/forEach.js
+++ Js code snippet/forEach.js
@@ -9,3 +9,17 @@ user.forEach((name, index) => {
     console.log(`Hello ${name} , Your access key is ${key[index]}`);
   }, delay);
 });
+
+// for all //
+var username = ['alice', 'lewis', 'johnson'];
+var city = ['seoul', 'us', 'tokyo'];
+
+username.forEach((name, index) => {
+    users(name, city[index]);
+});
+
+function users(name, town){
+    setTimeout(() => {
+        console.log(`Welcome ${name}, You are from ${town}`);
+    }, 3000);
+}
