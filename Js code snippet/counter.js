0225d84b657b6f360b1fc21b08984b33249a12af
G <dream.creater96@gmail.com>
2024-12-18 10:10:16 -0500

    Update counter.js

--- Js code snippet/counter.js
+++ Js code snippet/counter.js
@@ -1,30 +1,12 @@
-// let count = 0;
-
-// function countAuto(){
-//     console.log(count);
-//     if(count === 10){
-//         console.log("Count finished");
-//         return;
-//     }
-//     count++;
-//     setTimeout(countAuto, 1200);
-// }
-// countAuto();
-
-let count = 1;
+let count = 0;
 
 function countAuto(){
-    //display with html DOM
-    document.getElementById('display').innerHTML = count;
-
-    //if count reach target, display message
-    if(count === 10){
-        document.getElementById('message').innerHTML = "Boom!"
-        //hide count
-        document.getElementById('display').style.display = "none";
+    console.log(count);
+     if(count === 10){
+         console.log("Count finished");
         return;
     }
-    count++;
-    setTimeout(countAuto, 800);
-}
-countAuto();
+     count++;
+     setTimeout(countAuto, 1200);
+ }
+ countAuto();
