362ea3cd37658e8a35b974026cc322b5cfb7d90a
G <dream.creater96@gmail.com>
2024-12-22 12:16:37 +0630

    object.js

---
+++ Js code snippet/object.js
@@ -0,0 +1,16 @@
+//api fetch async/callback //
+// function fetchData(url, options) {
+//   // options can be an object containing headers, method, etc.
+// }
+// fetchData("https://api.example.com", { method: "GET", headers: { "Content-Type": "application/json" } });
+
+-----------------------------------------
+
+// event handler //
+// let button = document.querySelector("button");
+// let eventHandlers = {
+//     clickHandler: function() {
+//         alert("Button clicked!");
+//     }
+// };
+// button.addEventListener("Click", eventHandlers.clickHandler);
